f7352e2d744dcdf98f5e5e5af8d5b7fe84008114
Erik Anders <erikanders1@web.de>
2018-06-05 19:11:18 +0200

    Layout und OnClick function added

--- app/src/main/java/com/example/anderse/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/anderse/myapplication/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.anderse.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun countMe(view: View) {
+        // Get the text view
+        //val showCountTextView = findViewById(R.id.textView4) as TextView
+        val countString = textView4.text.toString()
+        // Get the value of the text view.
+        //val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // Display the new value in the text view.
+        textView4.text = count.toString();
     }
 }
